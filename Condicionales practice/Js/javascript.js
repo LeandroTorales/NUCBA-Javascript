d7efd9c2d061f94a8c3ede86de2207cae6121b66
Leandro Torales <leandrotorales1234@gmail.com>
2022-08-23 21:17:39 -0300

    Primer commit, empezar practica de bucles

---
+++ Condicionales practice/Js/javascript.js
@@ -0,0 +1,59 @@
+/* 
+let nombre = "Leandro";
+let edad = 19;
+console.log(`Hola, mi nombre es ${nombre}, tengo ${edad} años`);
+
+nombre = "leandroooo";
+edad = 20;
+console.log(`Hola, mi nombre es ${nombre}, tengo ${edad} años`);
+
+let num1 = 2;
+let num2 = 6;
+console.log(num1 + num2);
+
+let numero = 15;
+
+numero = 25;
+console.log(numero);
+
+alert(nombre);
+
+let pedirValor = prompt("Dar valor");
+console.log(pedirValor);
+alert(`El valor ingresado es ${pedirValor}`);
+
+let pedirNombre1 = prompt("Dame un nombre");
+let pedirNombre2 = prompt("Dame un segundo nombre");
+alert(`El primer valor ingresado es ${pedirNombre1} y el segundo es ${pedirNombre2}`); 
+
+var nombre2 = "carlos";
+console.log(nombre2);
+
+let edad2 = prompt("Solicitar edad");
+if (edad2 >= 18) {
+  console.log("Soy mayor de edad");
+} else if (edad2 < 18) {
+  console.log("Soy menor de edad");
+}
+
+let valorNum1 = prompt("Solicitar numero");
+let valorNum2 = prompt("Solicitar numero");
+
+if (valorNum1 > valorNum2) {
+  console.log(`${valorNum1} es mayor que ${valorNum2}`);
+} else if (valorNum2 > valorNum1) {
+  console.log(`${valorNum2} es mayor que ${valorNum1}`);
+} else if (valorNum1 === valorNum2) {
+  console.log(`${valorNum1} y ${valorNum2} son iguales`);
+}
+
+let valor = prompt("Solicitar: alerta o consola");
+if (valor == "alerta") {
+  alert("alerta");
+} else if (valor == "consola") {
+  console.log("Soy un mensaje en la consola");
+} else {
+  alert("Me muestro en ambos lados");
+  console.log("Me muestro en ambos lados");
+}
+*/
