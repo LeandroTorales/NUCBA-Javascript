611c993a8aca98aeb0c52c3c32efbc33d8de0279
Leandro Torales <leandrotorales1234@gmail.com>
2022-09-02 21:35:17 -0300

    a

---
+++ Clase 5 closure y objetos/js.js
@@ -0,0 +1,38 @@
+/* class gato {
+  constructor(color, uñas, tamaño, edad) {
+    this.color = color;
+    this.uñas = uñas;
+    this.tamaño = tamaño;
+    this.edad = edad;
+  }
+}
+const felino = new gato("marron", "cortas", "grande", "18 años");
+console.log(`El gato es ${felino.color}, tiene las uñas ${felino.uñas}, es ${felino.tamaño} y tiene ${felino.edad}.`); */
+
+/* Se crea la clase perro, que es todo lo que engloba a las "Caracteristicas" del perro, moverse, comer, todo */
+/* class perro {
+  //Se define el constructor 
+  constructor(tamaño, raza, edad, dientes, peso, a) {
+    this.tamaño = tamaño;
+    this.raza = raza;
+    this.edad = edad;
+    this.dientes = dientes;
+    this.peso = peso;
+    this.a = a;
+  }
+  // Metodo para poner cuantos dias no comio 
+  darleDeComer = function (a) {
+    this.a = a;
+  };
+  // Funcion que retorna el valor del metodo y la muestra por consola 
+  getDarleDeComer = () => {
+    console.log(` El perro esta sin comer desde hace ${this.a} dias`);
+  };
+}
+ //Se crea el objeto 
+const perrito = new perro("grande", "pitbull", 18, true, "28kg", 0);
+// Se le asigna el valor al parametro "a" de la funcion darleDeComer 
+perrito.darleDeComer(414);
+// Se ejecuta la funcion que retorna el valor de la primera funcion 
+perrito.getDarleDeComer(); 
+*/
