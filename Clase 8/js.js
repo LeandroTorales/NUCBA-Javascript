1de4445206f4b9656bf6e68cfa56cda9ed7a09eb
Leandro Torales <leandrotorales1234@gmail.com>
2022-09-11 23:50:42 -0300

    seguir practicando

---
+++ Clase 8/js.js
@@ -0,0 +1,51 @@
+const arrayPalabras = ["Mansion", "marioneta", "trapo", "casa", "Patio"];
+/* const filtarM = (filtrado) => {
+  let nuevoArrayPalabras = [];
+  filtrado.forEach((element) => {
+    if (element.toLowerCase().includes("m")) {
+      nuevoArrayPalabras.push(element);
+    }
+  });
+  return nuevoArrayPalabras;
+};
+
+console.log(filtarM(arrayPalabras)); */
+
+const arrayNumeros1 = [1, 24, 41, 7, 845, 4, 3566, 351, 9];
+
+/* const filtrarNumeros = (a, b, filtrar) => {
+  let nuevoArrayNumeros = [];
+  filtrar.forEach((filtrar) => {
+    if (filtrar > a && filtrar < b) {
+      nuevoArrayNumeros.push(filtrar);
+    }
+  });
+  return nuevoArrayNumeros;
+};
+console.log("filtrarNumeros:", filtrarNumeros1(10, 800, arrayNumeros)); */
+
+const arrayNumeros2 = [1, 2, 3, 4, 5];
+
+/* const multiplicarAlgunosNumeros = (a, b, filtrado) => {
+  let nuevoArrayNumeros = [];
+  filtrado.forEach((filtrado) => {
+    if (filtrado > a) {
+      nuevoArrayNumeros.push(filtrado * b);
+    } else if (filtrado <= a) {
+      nuevoArrayNumeros.push(filtrado);
+    }
+  });
+  return nuevoArrayNumeros;
+};
+
+console.log(multiplicarAlgunosNumeros(3, 10, arrayNumeros2)); */
+/* 
+const encontrarNumero = (array, n) => {
+  if (array.some((numero) => numero > n)) {
+    return array.find((numero) => numero > n);
+  } else {
+    return "No hay numeros coincidientes";
+  }
+};
+console.log("encontrarNumero:", encontrarNumero(arrayNumeros2, 3));
+ */
