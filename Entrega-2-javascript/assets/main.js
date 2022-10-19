96acf5a7f377bde858535d707f0104fb48eca0b1
Leandro Federico Torales <leandrotorales1234@gmail.com>
2022-10-19 00:42:31 -0300

    entrega 2 finish

--- Entrega-2-javascript/assets/main.js
+++ Entrega-2-javascript/assets/main.js
@@ -1,11 +1,51 @@
 const form = document.getElementById("form");
 const cardsContainer = document.getElementById("cards__renders--containers");
 
-let item = JSON.parse(localStorage.getItem("item")) || [];
+let pizzas = JSON.parse(localStorage.getItem("pizzas")) || [];
 const saveLocalStorage = () => {
-  localStorage.setItem("items", JSON.stringify(item));
+  localStorage.setItem("pizzas", JSON.stringify(pizzas));
 };
 
-const saveItems = () => {
-    
-}
+const saveData = () => {
+  const inputValue = inputNumber.value;
+  const findPizza = pizzasArray.find((e) => e.id == inputValue) || [];
+  pizzas = [
+    ...pizzas,
+    {
+      id: findPizza.id,
+      nombre: findPizza.nombre,
+      precio: findPizza.precio,
+    },
+  ];
+};
+
+const renderContainerPizza = (pizza) => {
+  const { id, nombre, ingredientes, precio } = pizza;
+  return `
+          <div class="render__succes">
+            <h2>Pizza ${nombre}, con id ${id}</h2>
+            <h3>El precio de la pizza es: <span>$${precio}</span></h3>
+          </div>
+          `;
+};
+
+const renderPizza = () => {
+  cardsContainer.innerHTML = pizzas.map(renderContainerPizza).join("");
+};
+
+const submitForm = (e) => {
+  e.preventDefault();
+  if (isValidForm()) {
+    saveData();
+    saveLocalStorage();
+    renderPizza();
+    form.reset();
+  }
+};
+
+const init = () => {
+  window.addEventListener("DOMContentLoaded", renderPizza(pizzas));
+  form.addEventListener("submit", submitForm);
+};
+
+init();
