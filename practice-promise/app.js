95a4c33913350af312373a7c39b59ebecd3db11d
Leandro Federico Torales <leandrotorales1234@gmail.com>
2022-10-30 22:02:55 -0300

    practice promise

--- practice-promise/app.js
+++ practice-promise/app.js
@@ -1,8 +1,9 @@
-const urlApiPokemon = "https://pokeapi.co/api/v2/pokemon/";
-const promesaFetch = (url) => {
-  fetch(url)
-    .then((resolve) => resolve.json())
-    .then((resolve) => console.log(resolve.results))
-    .catch((error) => console.log(error));
+const urlApi = "https://reqres.in/api/users?page=1";
+const promesaFetch = async (url) => {
+  const apiFetch = await fetch(url);
+  const results = await apiFetch.json();
+  console.log(results.data);
+  const resultForEach = results.data.forEach((persona) => console.log(`Hola, me llamo:${persona.first_name}, mi email es: ${persona.email}`));
+  return results;
 };
-promesaFetch(urlApiPokemon);
+console.log(promesaFetch(urlApi));
