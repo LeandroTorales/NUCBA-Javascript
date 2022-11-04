311f2f24f9a0ed37e4a20e2afabe63200a334ce9
Leandro Federico Torales <leandrotorales1234@gmail.com>
2022-11-04 00:22:12 -0300

    terminar mediaQuerys y responsive del input, empezar logica

---
+++ entrega-4-javascript/assets/main.js
@@ -0,0 +1,4 @@
+const form = document.getElementById("form");
+const containerResults = document.querySelector(".container-result");
+const input = document.getElementById("input");
+const inputValue = input.value;
